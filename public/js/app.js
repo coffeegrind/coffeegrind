5127c94b518e462452373e3c8ea903ab0744f9df
Nick Aversano <nick23hi@gmail.com>
2016-03-12 00:52:06 -0500

    little things

--- public/js/app.js
+++ public/js/app.js
@@ -17,3 +17,30 @@ document.addEventListener('dragover', preventDrag, false);
     });
   });
 })(jQuery);
+
+
+var remote = require('remote')
+var Menu = remote.require('menu')
+var MenuItem = remote.require('menu-item')
+
+// Build our new menu
+var menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Delete',
+  click: function() {
+    alert('Deleted')
+  }
+}))
+menu.append(new MenuItem({
+  label: 'More Info...',
+  click: function() {
+    alert('Here is more information')
+  }
+}))
+
+// Add the listener
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.js-context-menu').addEventListener('contextmenu', function (event) {
+    menu.popup(remote.getCurrentWindow());
+  })
+})
