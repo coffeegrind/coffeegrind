479ed2d9fd97b13448f12fc6cd28f5d3994ef09c
Nick Aversano <nick23hi@gmail.com>
2016-03-24 12:07:54 -0400

    timing working, but ui buggy

--- public/js/app.js
+++ public/js/app.js
@@ -8,14 +8,21 @@ document.addEventListener('drop', preventDrag, false);
 document.addEventListener('dragenter', preventDrag, false);
 document.addEventListener('dragover', preventDrag, false);
 
-(function($) {
+function init() {
+  var $curr = false;
   // blue highlights
   $('ul li').click(function(e) {
-    var $this = $(this);
+    var $this = $curr = $(this);
     $this.addClass('active');
+    controller.use($this.attr('data-id'));
     $this.siblings().each(function(i, e){
       $(e).removeClass('active');
     });
+  });
+  
+  $('input').focus(function(e) {
+    $curr = false;
+    $('ul li').removeClass('active');
   });
   
   $(document).keydown(function(e) {
@@ -24,7 +31,17 @@ document.addEventListener('dragover', preventDrag, false);
       e.preventDefault();
       return false;
     }*/
+    // space or enter to start stop the active project
+    if ($curr && (e.which == 32 || e.which == 13)) {
+      $curr.toggleClass('record', controller.toggle());
+    }
   });
+  
+  // poll for updates
+  setInterval(function() {
+    if (!$curr) return;
+    $curr.find('.time').text(controller.activeProject.getHumanTime());
+  }, 500);
   
   // on-page keyboard shortcuts
   var Keyboard = require('./js/keyboard');
@@ -47,12 +64,21 @@ document.addEventListener('dragover', preventDrag, false);
   
   // submit form
   $(create).submit(function(e) {
-    console.log($(this).find('input').val());
+    var $input = $(this).find('input');
+    var val = $input.val();
+    controller.use(val);
+    var p = controller.activeProject;
+    
+    var $newLi = $('<li class="list-group-item" data-id="' + p.name + '"><strong>' + p.name + '</strong><span class="time pull-right">' + p.getHumanTime() + '</span></li>');
+    $projects.prepend($newLi);
+    $newLi.click();
+    
+    $input.val('').blur();
+    
     e.preventDefault();
     return false;
   });
-})(jQuery);
-
+}
 
 var remote = require('remote')
 var Menu = remote.require('menu')
@@ -66,10 +92,10 @@ const controller = new ProjectController(storage);
 
 var $projects = $(projects);
 controller.getProjects().forEach(function(e, i) {
-  // check for valid projects
-  if (!e.name) return;
-  $projects.append($('<li class="list-group-item"><strong>' + e.name + '</strong><span class="time pull-right">' + e.getHumanTime() + '</span><p>Lorem ipsum dolor sit amet.</p></li>'));
+  $projects.append($('<li class="list-group-item" data-id="' + e.name + '"><strong>' + e.name + '</strong><span class="time pull-right">' + e.getHumanTime() + '</span></li>'));
 });
+
+init();
 
 // Build our new menu
 var menu = new Menu()
